74eabbd504192251ffb4dfb9a4eb3a3c3570ee69
Michael Glienecke <michael@glienecke.de>
2023-10-11 16:43:36 +0100

    Initial commit

---
+++ src/main/java/uk/ac/ed/inf/ilptutorialrestservice/IlpTutorialRestServiceApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.ed.inf.ilptutorialrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlpTutorialRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlpTutorialRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/ed/inf/ilptutorialrestservice/IlpTutorialRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ed.inf.ilptutorialrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlpTutorialRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
